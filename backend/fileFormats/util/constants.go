183afcf1ad3bc91e81db0b437fffc13adaae49f8
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-11-29 18:42:25 -0300

    feat: remover a tabela de caracteres e suas constantes associadas

--- backend/fileFormats/util/constants.go
+++ backend/fileFormats/util/constants.go
@@ -1,16 +1,9 @@
 package util
 
-const CHARACTER_TABLE_RESOURCES_DIR string = "tbs"
-const FFX_ENCODING_TABLE_NAME string = "ffx.tbs"
-const FFX2_ENCODING_TABLE_NAME string = "ffx2.tbs"
-const CHARACTER_ENCODING_TABLE string = "ffx_chars.tbs"
-const CHARACTER_LOC_ENCODING_TABLE string = "ffxloc.tbs"
-
 const UTILS_RESOURCES_DIR string = "utils"
 const DCP_FILE_XPLITTER_APPLICATION string = "SHSplit.exe"
 
 const DEFAULT_APPLICATION_FILE_EXTENSION string = ".exe"
-const DEFAULT_ENCODING_TABLE_FILE_EXTENSION string = ".tbs"
 
 const DCP_TXT_PARTS_PATTERN string = dcp_file_pattern + "\\.txt"
 const DCP_FILE_PARTS_PATTERN string = dcp_file_pattern + "$"
