6dcee3118cdb0f5df7832a1f546171b1513708dc
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-11-29 15:22:12 -0300

    feat: adicionar codificador de textos com suporte a Dlg e Krnl

---
+++ backend/fileFormats/internal/text/encoding/ffx_texts_encoder.go
@@ -0,0 +1,66 @@
+package textsEncoding
+
+import (
+	"ffxresources/backend/common"
+	"ffxresources/backend/core/encoding"
+	"ffxresources/backend/lib"
+	"fmt"
+)
+
+type textsEncoder struct{}
+
+func NewEncoder() *textsEncoder {
+	return &textsEncoder{}
+}
+
+func (d *textsEncoder) DlgEncoder(sourceFile, targetFile, outputFile string, encoding ffxencoding.IFFXTextDlgEncoding) error {
+	encodingFile := encoding.FetchEncoding()
+
+	executable, err := encoding.FetchDlgHandler().FetchDlgTextsHandler()
+	if err != nil {
+		return err
+	}
+
+	if err := d.encoder(executable, sourceFile, targetFile, outputFile, encodingFile); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *textsEncoder) KnrlEncoder(sourceFile, targetFile, outputFile string, encoding ffxencoding.IFFXTextKrnlEncoding) error {
+	encodingFile := encoding.FetchEncoding()
+
+	executable, err := encoding.FetchKrnlHandler().FetchKrnlTextsHandler()
+	if err != nil {
+		return err
+	}
+
+	if err := d.encoder(executable, sourceFile, targetFile, outputFile, encodingFile); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *textsEncoder) encoder(executable, sourceFile, targetFile, outputFile, encoding string) error {
+	if !common.IsFileExists(sourceFile) {
+		return fmt.Errorf("source file does not exist")
+	}
+
+	if !common.IsFileExists(targetFile) {
+		return fmt.Errorf("target file does not exist")
+	}
+
+	if !common.IsFileExists(encoding) {
+		return fmt.Errorf("encoding file does not exist")
+	}
+
+	args := []string{"-i", "-t", encoding, sourceFile, targetFile, outputFile}
+
+	if err := lib.RunCommand(executable, args); err != nil {
+		return err
+	}
+
+	return nil
+}
