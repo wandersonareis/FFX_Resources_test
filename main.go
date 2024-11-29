4d12edf1c07d97bfa72cbceabbe869e631fc4598
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-11-29 14:25:40 -0300

    feat: adicionar tratamento de pânico no main para melhorar a robustez da aplicação

--- main.go
+++ main.go
@@ -45,6 +45,12 @@ func main() {
 		}
 	}()
 
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r.(error).Error())
+		}
+	}()
+
 	logToFile()
 	app := NewApp()
 	
