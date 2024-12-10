79917085d7ac400cfb94a441d6fecc8e4593d663
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2024-12-09 22:49:26 -0300

    feat: adicionar tratamento de pânico e notificação de erro na extração de arquivos

--- backend/services/extract.service.go
+++ backend/services/extract.service.go
@@ -4,6 +4,7 @@ import (
 	"ffxresources/backend/fileFormats"
 	"ffxresources/backend/interactions"
 	"ffxresources/backend/logger"
+	"ffxresources/backend/notifications"
 	"fmt"
 )
 
@@ -14,12 +15,29 @@ func NewExtractService() *ExtractService {
 }
 
 func (e *ExtractService) Extract(dataInfo *interactions.GameDataInfo) {
+	defer func() {
+		if r := recover(); r != nil {
+			l := logger.Get()
+			l.Error().
+				Interface("recover", r).
+				Str("file", dataInfo.GameData.Name).
+				Msg("Panic occurred during extraction")
+
+			notifications.NotifyError(fmt.Errorf("panic occurred: %v", r))
+		}
+	}()
+
 	fileProcessor := fileFormats.NewFileProcessor(dataInfo)
 	if fileProcessor == nil {
 		l := logger.Get()
-		l.Error().Err(fmt.Errorf("invalid file type: %s", dataInfo.GameData.Name)).Msg("Error extracting file")
+		l.Error().
+			Err(fmt.Errorf("invalid file type: %s", dataInfo.GameData.Name)).
+			Msg("Error extracting file")
+			
 		return
 	}
-	
-	fileProcessor.Extract()
+
+	if err := fileProcessor.Extract(); err != nil {
+		notifications.NotifyError(err)
+	}
 }
