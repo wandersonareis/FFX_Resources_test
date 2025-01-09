45731983abe868bc1a6872c8df453ae48a2d792e
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-01-09 01:22:24 -0300

    refactor: otimizar a função NewSpiraFileInfo e melhorar a legibilidade do código

--- backend/core/spira_fileinfo.go
+++ backend/core/spira_fileinfo.go
@@ -23,56 +23,58 @@ type SpiraFileInfo struct {
 }
 
 func getDirSize(path string) (int64, error) {
-    var size int64
-    err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
-        if err != nil {
-            return err
-        }
-        if !d.IsDir() {
-            info, err := d.Info()
-            if err != nil {
-                return err
-            }
-            size += info.Size()
-        }
-        return nil
-    })
-    return size, err
+	var size int64
+	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			info, err := d.Info()
+			if err != nil {
+				return err
+			}
+			size += info.Size()
+		}
+		return nil
+	})
+	return size, err
 }
 
 func NewSpiraFileInfo(path string, gamePart models.GameVersion) (*SpiraFileInfo, error) {
-    cPath := filepath.Clean(path)
-    info, err := os.Stat(cPath)
-    if err != nil {
-        return nil, err
-    }
+	cPath := filepath.Clean(path)
+	info, err := os.Stat(cPath)
+	if err != nil {
+		return nil, err
+	}
 
-    var size int64
-    if info != nil {
-        if info.IsDir() {
-            size, err = getDirSize(cPath)
-            if err != nil {
-                return nil, err
-            }
-        } else {
-            size = info.Size()
-        }
-    }
+	var size int64
+	if info != nil {
+		if info.IsDir() {
+			size, err = getDirSize(cPath)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			size = info.Size()
+		}
+	}
 
-    source := &SpiraFileInfo{
-        Path:         cPath,
-        Size:         size,
-        Name:         common.RecursiveRemoveFileExtension(info.Name()),
-        NamePrefix:   common.RemoveOneFileExtension(info.Name()),
-        Type:         guessFileType(cPath),
-        Extension:    filepath.Ext(cPath),
-        EntryPath:    filepath.Join(cPath, info.Name()),
-        Parent:       filepath.Dir(cPath),
-        IsDir:        info.IsDir(),
-        RelativePath: common.GetRelativePathFromMarker(cPath),
-    }
+	source := &SpiraFileInfo{
+		Path:       cPath,
+		Size:       size,
+		Name:       common.RecursiveRemoveFileExtension(info.Name()),
+		NamePrefix: common.RemoveOneFileExtension(info.Name()),
+		Type:       guessFileType(cPath),
+		Extension:  filepath.Ext(cPath),
+		EntryPath:  cPath,
+		Parent:     filepath.Dir(cPath),
+		IsDir:      info.IsDir(),
+	}
 
-    return source, nil
+	if source.Type != models.Dcp && source.Type != models.Lockit {
+		source.RelativePath = common.GetRelativePathFromMarker(cPath)
+	}
+	return source, nil
 }
 
 func (s *SpiraFileInfo) ReadDir() ([]fs.DirEntry, error) {
