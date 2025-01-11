567156a7156a8729e7812f1d9b0225522abbb275
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-01-11 19:52:01 -0300

    refactor: substituir nodeMap por NodeMap e ajustar referências no CollectionService

--- backend/services/collection.service.go
+++ backend/services/collection.service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"ffxresources/backend/fileFormats"
 	"ffxresources/backend/formatters"
 	"ffxresources/backend/interactions"
 	"ffxresources/backend/notifications"
@@ -11,7 +12,7 @@ import (
 
 type CollectionService struct{}
 
-var nodeMap spira.TreeMapNode
+var NodeMap fileFormats.TreeMapNode
 
 func NewCollectionService() *CollectionService {
 	return &CollectionService{}
@@ -21,10 +22,6 @@ func (c *CollectionService) BuildTree() []spira.TreeNode {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	beforeAlloc := m.Alloc
-
-	/* errChan := make(chan error)
-	go notifications.PanicRecover(errChan, logger.Get().With().Str("service", "collection").Logger())
-	defer close(errChan) */
 
 	path := interactions.NewInteractionService().GameLocation.GetTargetDirectory()
 	if path == "" {
@@ -36,36 +33,18 @@ func (c *CollectionService) BuildTree() []spira.TreeNode {
 		return nil
 	}
 
-	/* source, err := locations.NewSource(path, interactions.NewInteractionService().FFXGameVersion().GetGameVersion())
-	if err != nil {
-		notifications.NotifyError(err)
-		return nil
-	} */
-
 	formatter := formatters.NewTxtFormatter()
-
-	//destination := locations.NewDestination()
-	//destination.InitializeLocations(source, formatter)
-
-	//tree := components.NewEmptyList[spira.TreeNode]()
 
 	gameVersion := interactions.NewInteractionService().FFXGameVersion().GetGameVersion()
 
-	/* if err := spira.BuildFileTree(tree, source, gameVersion, formatter); err != nil {
-		notifications.NotifyError(err)
-		return nil
-	} */
-
-	nodeMap = spira.CreateFileTreeMap(gameVersion, formatter)
-	rootTreeNode := spira.BuildTreeFromMap(nodeMap, path)
+	NodeMap = spira.CreateFileTreeMap(gameVersion, formatter)
+	rootTreeNode := spira.BuildTreeFromMap(NodeMap, path)
 
 	runtime.ReadMemStats(&m)
 	afterAlloc := m.Alloc
-	
 
 	fmt.Printf("Memory allocated: %d bytes\n", afterAlloc-beforeAlloc)
 	fmt.Printf("Total allocations: %d\n", m.Mallocs-m.Frees)
-
 
 	return []spira.TreeNode{*rootTreeNode}
 }
