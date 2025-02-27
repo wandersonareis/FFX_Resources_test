477f99185b3a4fa9361be6b4e8f452278ba5fb91
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-02-27 20:31:22 -0300

    dcp file test

--- backend/fileFormats/internal/dcp/dcp_file_test.go
+++ backend/fileFormats/internal/dcp/dcp_file_test.go
@@ -5,10 +5,12 @@ import (
 	"ffxresources/backend/core"
 	"ffxresources/backend/core/locations"
 	"ffxresources/backend/core/locations/locationsBase"
-	"ffxresources/backend/fileFormats/internal/base"
+	"ffxresources/backend/fileFormats/internal/baseFormats"
+	"ffxresources/backend/fileFormats/internal/dcp/internal/lib"
 	"ffxresources/backend/formatters"
 	"ffxresources/backend/interactions"
 	"ffxresources/backend/logger"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -18,18 +20,26 @@ import (
 func TestNewDcpFile(t *testing.T) {
 	assert := assert.New(t)
 
+	var interactionService *interactions.InteractionService
+
+	t.Run("Set APP_BASE_PATH", func(t *testing.T) {
+		err := os.Setenv("APP_BASE_PATH", `F:\ffxWails\FFX_Resources\build\bin`)
+		assert.Nil(err)
+	})
+
 	testPath := `F:\ffxWails\FFX_Resources\build\bin\data\ffx_ps2\ffx2\master\new_uspc\menu\macrodic.dcp`
 	currentDir, err := filepath.Abs(".")
 	assert.Nil(err)
 
-	gameVersionDir := "FFX-2"
-	extractTempPath := filepath.Join(common.GetTempDir(), "extract")
+	temp := common.NewTempProvider("", "")
+
+	extractTempPath := filepath.Join(temp.TempFilePath, "extract")
 	defer func() {
 		err = common.RemoveDir(extractTempPath)
 		assert.Nil(err)
 	}()
 
-	reimportTempPath := filepath.Join(common.GetTempDir(), "reimport")
+	reimportTempPath := filepath.Join(temp.TempFilePath, "reimport")
 	translatePath := filepath.Join(currentDir, "/testData/")
 
 	config := &interactions.FFXAppConfig{
@@ -39,19 +49,32 @@ func TestNewDcpFile(t *testing.T) {
 		TranslateLocation: translatePath,
 		ImportLocation:    reimportTempPath,
 	}
+	t.Run("Config is not nil", func(t *testing.T) {
+		assert.NotNil(config)
+	})
 
-	interactionService := interactions.NewInteractionServiceWithConfig(config)
-	assert.NotNil(interactionService)
+	t.Run("Initialize Interaction Service", func(t *testing.T) {
+		interactionService = interactions.NewInteractionServiceWithConfig(config)
+		assert.NotNil(interactionService)
+		assert.NotNil(interactionService.FFXAppConfig())
+	})
+
+	gameVersionDir := "FFX-2"
 
 	formatter := &formatters.TxtFormatter{
 		TargetExtension: ".txt",
 		GameVersionDir:  gameVersionDir,
 		GameFilesPath:   translatePath,
 	}
-	assert.NotNil(formatter)
+	t.Run("Formatter is not nil", func(t *testing.T) {
+		assert.NotNil(formatter)
+	})
 
 	interactionService = interactions.NewInteractionWithTextFormatter(formatter)
-	assert.NotNil(interactionService)
+
+	t.Run("Interaction service formatter is not nil", func(t *testing.T) {
+		assert.NotNil(interactionService.TextFormatter())
+	})
 
 	source, err := locations.NewSource(testPath)
 	assert.Nil(err)
@@ -61,12 +84,26 @@ func TestNewDcpFile(t *testing.T) {
 		TranslateLocation: locations.NewTranslateLocationInfo(locationsBase.WithDirectoryName("translated"), locationsBase.WithTargetDirectory(translatePath), locationsBase.WithGameVersionDir(gameVersionDir)),
 		ImportLocation:    locations.NewImportLocationInfo(locationsBase.WithDirectoryName("reimported"), locationsBase.WithTargetDirectory(reimportTempPath), locationsBase.WithGameVersionDir(gameVersionDir)),
 	}
-	assert.NotNil(destination)
-
+	
+	t.Run("Destination is not nil", func(t *testing.T) {
+		assert.NotNil(destination)
+	})
+	
 	destination.InitializeLocations(source, formatter)
 
+	t.Run("Destination extract location path is temporary path", func(t *testing.T) {
+		expected := filepath.Join(extractTempPath, gameVersionDir, lib.DCP_PARTS_TARGET_DIR_NAME)
+		expected = filepath.ToSlash(expected)
+
+		actual := destination.Extract().Get().GetTargetPath()
+		actual = filepath.ToSlash(actual)
+
+		assert.Equal(expected, actual)
+	})
+
+
 	dcpFile := &DcpFile{
-		FormatsBase: &base.FormatsBase{
+		IBaseFileFormat: &baseFormats.BaseFileFormat{
 			Source:      source,
 			Destination: destination,
 		},
