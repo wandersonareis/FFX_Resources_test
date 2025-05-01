b8e97003ba47b8b39221b36ec09c1353ab2e0ad6
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-04-30 23:32:19 -0300

    feat: refatorar DialogExtractor para melhorar a estrutura e nomenclatura

--- backend/fileFormats/internal/text/internal/dlg/dialogs_file_extractor.go
+++ backend/fileFormats/internal/text/internal/dlg/dialogs_file_extractor.go
@@ -14,28 +14,28 @@ type (
 		Extract(source interfaces.ISource, destination locations.IDestination) error
 	}
 
-	dlgExtractor struct {
-		decoder internal.IDlgDecoder
-		logger logger.ILoggerHandler
+	DialogExtractor struct {
+		DialogDecoder internal.IDlgDecoder
+		Logger        logger.ILoggerHandler
 	}
 )
 
 func NewDlgExtractor(logger logger.ILoggerHandler) IDlgExtractor {
-	return &dlgExtractor{
-		decoder: internal.NewDlgDecoder(logger),
-		logger: logger,
+	return &DialogExtractor{
+		DialogDecoder: internal.NewDlgDecoder(logger),
+		Logger:        logger,
 	}
 }
 
-func (d *dlgExtractor) Extract(source interfaces.ISource, destination locations.IDestination) error {
+func (d *DialogExtractor) Extract(source interfaces.ISource, destination locations.IDestination) error {
 	if err := destination.Extract().ProvideTargetDirectory(); err != nil {
 		return fmt.Errorf("failed to provide target directory: %s", err)
 	}
-	
+
 	textEncoding := ffxencoding.NewFFXTextEncodingFactory().CreateFFXTextDlgEncoding(source.Get().Type)
 	defer textEncoding.Dispose()
 
-	if err := d.decoder.Decoder(source, destination, textEncoding); err != nil {
+	if err := d.DialogDecoder.Decoder(source, destination, textEncoding); err != nil {
 		return fmt.Errorf("failed to decode dialog file: %s", source.Get().Name)
 	}
 
