4d208c71ed52ce1cb61fc323bbbf637ec3fe845c
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-04-30 23:32:29 -0300

    feat: refatorar testes de DialogFile para melhorar a legibilidade e a estrutura

--- backend/fileFormats/internal/text/dialogs_file_test.go
+++ backend/fileFormats/internal/text/dialogs_file_test.go
@@ -1,11 +1,9 @@
 package text_test
 
 import (
-	"bufio"
 	"ffxresources/backend/common"
 	ffxencoding "ffxresources/backend/core/encoding"
 	"ffxresources/backend/core/locations"
-	"ffxresources/backend/core/locations/locationsBase"
 	"ffxresources/backend/fileFormats/internal/text"
 	"ffxresources/backend/fileFormats/internal/text/internal/dlg"
 	"ffxresources/backend/fileFormats/internal/text/internal/lib"
@@ -13,13 +11,14 @@ import (
 	"ffxresources/backend/formatters"
 	"ffxresources/backend/interactions"
 	"ffxresources/backend/interfaces"
-	"ffxresources/backend/logger"
 	"ffxresources/backend/models"
-	"ffxresources/testData"
+	testcommon "ffxresources/testData"
 	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -48,7 +47,7 @@ var _ = Describe("DlgFile", Ordered, func() {
 		config              *interactions.FFXAppConfig
 		mockNotifierService *testcommon.MockNotifier
 		temp                *common.TempProvider
-		log                 logger.ILoggerHandler
+		log                 *testcommon.MockLogHandler
 	)
 
 	BeforeAll(func() {
@@ -86,13 +85,13 @@ var _ = Describe("DlgFile", Ordered, func() {
 		interactions.NewInteractionServiceWithConfig(config)
 		interactions.NewInteractionWithTextFormatter(formatter)
 
-		log = logger.NewLoggerHandler("dlg_file_test")
+		log = testcommon.NewLogHandlerMock()
 
 		// Setup destination
 		destination = &locations.Destination{
-			ExtractLocation:   locations.NewExtractLocationInfo(locationsBase.WithDirectoryName("extracted"), locationsBase.WithTargetDirectory(extractTempPath), locationsBase.WithGameVersionDir(gameVersionDir)),
-			TranslateLocation: locations.NewTranslateLocationInfo(locationsBase.WithDirectoryName("translated"), locationsBase.WithTargetDirectory(translatePath), locationsBase.WithGameVersionDir(gameVersionDir)),
-			ImportLocation:    locations.NewImportLocationInfo(locationsBase.WithDirectoryName("reimported"), locationsBase.WithTargetDirectory(reimportTempPath), locationsBase.WithGameVersionDir(gameVersionDir)),
+			ExtractLocation:   locations.NewExtractLocation("extracted", extractTempPath, gameVersionDir),
+			TranslateLocation: locations.NewTranslateLocation("translated", translatePath, gameVersionDir),
+			ImportLocation:    locations.NewImportLocation("reimported", reimportTempPath, gameVersionDir),
 		}
 	})
 
@@ -114,33 +113,7 @@ var _ = Describe("DlgFile", Ordered, func() {
 		Expect(common.RemoveDir(temp.TempFilePath)).To(Succeed())
 	})
 
-	Describe("Miscellaneous Functionality", func() {
-		It("should have valid game version for FFX-2", func() {
-			Expect(os.Getenv("FFX_GAME_VERSION")).To(Equal("2"))
-		})
-
-		It("should have valid configuration", func() {
-			Expect(config).NotTo(BeNil())
-		})
-
-		It("should have valid formatter", func() {
-			Expect(formatter).NotTo(BeNil())
-		})
-
-		It("should have interaction service with text formatter", func() {
-			Expect(interactions.NewInteractionService().TextFormatter()).NotTo(BeNil())
-		})
-
-		It("should have valid destination", func() {
-			Expect(destination).NotTo(BeNil())
-		})
-
-		It("should have valid logger", func() {
-			Expect(log).NotTo(BeNil())
-		})
-	})
-
-	Describe("Clones functionality", func() {
+	Context("Clones functionality", func() {
 		var checkClones = func(relPath string, expectedCount int) {
 			testFilePath := filepath.Join(gameLocationPath, relPath)
 			Expect(common.CheckPathExists(testFilePath)).To(Succeed())
@@ -168,6 +141,11 @@ var _ = Describe("DlgFile", Ordered, func() {
 	})
 
 	Context("Dialogs Extract Functionality", func() {
+		BeforeEach(func() {
+			testDlgExtractor = dlg.NewDlgExtractor(log)
+			Expect(testDlgExtractor).NotTo(BeNil())
+		})
+
 		var extractFile = func(fileRelPath string, fileType models.NodeType) {
 			testFilePath := filepath.Join(gameLocationPath, fileRelPath)
 			Expect(common.CheckPathExists(testFilePath)).To(Succeed())
@@ -178,15 +156,9 @@ var _ = Describe("DlgFile", Ordered, func() {
 			Expect(source.Get().Type).To(Equal(fileType))
 
 			Expect(destination).NotTo(BeNil())
-
 			destination.InitializeLocations(source, formatter)
 
 			Expect(testDlgExtractor.Extract(source, destination)).To(Succeed())
-
-			// Verify the extracted file
-			Expect(testDlgVerifyer).NotTo(BeNil())
-			Expect(testDlgVerifyer.Verify(source, destination, textVerifier.ExtractIntegrityCheck)).To(Succeed())
-			Expect(common.CheckPathExists(destination.Extract().Get().GetTargetFile())).To(Succeed())
 		}
 
 		It("should extract the bika07_235.bin successfully", func() {
@@ -208,6 +180,31 @@ var _ = Describe("DlgFile", Ordered, func() {
 		It("should extract variant event file credits.bin successfully", func() {
 			extractFile(`ffx_ps2\ffx2\master\new_uspc\event\obj_ps3\cr\credits\credits.bin`, models.DialogsSpecial)
 		})
+
+		It("should throw error", func() {
+			file := `ffx_ps2\ffx2\master\new_uspc\battle\btl\bika07_235\bika07_235.bin`
+			testFilePath := filepath.Join(gameLocationPath, file)
+			Expect(common.CheckPathExists(testFilePath)).To(Succeed())
+
+			source, err := locations.NewSource(testFilePath)
+			Expect(err).To(BeNil())
+			Expect(source).NotTo(BeNil())
+
+			Expect(destination).NotTo(BeNil())
+
+			destination.InitializeLocations(source, formatter)
+
+			m := new(testcommon.MockDlgDecoder)
+			m.On("Decoder", source, destination, mock.Anything).Return(fmt.Errorf("mock error"))
+
+			mockDlgDecoder := dlg.DialogExtractor{
+				DialogDecoder: m,
+				Logger:        log,
+			}
+
+			err = mockDlgDecoder.Extract(source, destination)
+			Expect(err).To(HaveOccurred())
+		})
 	})
 
 	Context("Dialogs Compress Functionality", func() {
@@ -218,15 +215,15 @@ var _ = Describe("DlgFile", Ordered, func() {
 		var checkClones = func(fileClones []string) {
 			count := 0
 			for _, clone := range fileClones {
-				destinationPath := destination.Import().Get().GetTargetDirectory()
+				destinationPath := destination.Import().GetTargetDirectory()
 				cloneFilePath := filepath.Join(destinationPath, clone)
 				Expect(common.CheckPathExists(cloneFilePath)).To(Succeed(), "Clone file should exist: %s", cloneFilePath)
 
 				count++
-				mockNotifierService.NotifySuccess(fmt.Sprintf("Clones: %d/%d", count, len(fileClones)))
 			}
 
 			Expect(count).To(Equal(len(fileClones)), "All clones should be processed")
+			mockNotifierService.NotifySuccess(fmt.Sprintf("Clones: %d/%d", count, len(fileClones)))
 		}
 
 		var compressClones = func(relPath string) {
@@ -242,7 +239,7 @@ var _ = Describe("DlgFile", Ordered, func() {
 			source.PopulateDuplicatesFiles(gameVersion)
 
 			Expect(testDlgCompressor.Compress(source, destination)).To(Succeed())
-			Expect(common.CheckPathExists(destination.Import().Get().GetTargetFile())).To(Succeed())
+			Expect(common.CheckPathExists(destination.Import().GetTargetFile())).To(Succeed())
 
 			if source.Get().ClonedItems != nil {
 				checkClones(source.Get().ClonedItems)
@@ -271,44 +268,6 @@ var _ = Describe("DlgFile", Ordered, func() {
 	})
 
 	Describe("Verifyer Functionality", func() {
-		removeFirstNLines := func(filePath string, n int) error {
-			file, err := os.Open(filePath)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-
-			var remainingLines []string
-			scanner := bufio.NewScanner(file)
-
-			lineNumber := 0
-			for scanner.Scan() {
-				if lineNumber >= n {
-					remainingLines = append(remainingLines, scanner.Text())
-				}
-				lineNumber++
-			}
-
-			if err := scanner.Err(); err != nil {
-				return err
-			}
-
-			file, err = os.Create(filePath)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-
-			writer := bufio.NewWriter(file)
-			for _, line := range remainingLines {
-				_, err := writer.WriteString(line + "\n")
-				if err != nil {
-					return err
-				}
-			}
-
-			return writer.Flush()
-		}
 
 		It("should verify file integrity binary fail", func() {
 			testPath := `binary\ffx_ps2\ffx2\master\new_uspc\cloudsave\cloud.bin`
@@ -329,13 +288,13 @@ var _ = Describe("DlgFile", Ordered, func() {
 			dlgIntegrity := textVerifier.NewTextsVerify(log)
 			Expect(dlgIntegrity).NotTo(BeNil())
 
-			Expect(removeFirstNLines(destination.Extract().Get().GetTargetFile(), 4)).To(Succeed())
+			Expect(testcommon.RemoveFirstNLines(destination.Extract().GetTargetFile(), 4)).To(Succeed())
 
 			err = dlgIntegrity.Verify(source, destination, textVerifier.ExtractIntegrityCheck)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("source and target segments count mismatch"))
 
-			err = common.CheckPathExists(destination.Extract().Get().GetTargetFile())
+			err = common.CheckPathExists(destination.Extract().GetTargetFile())
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("path does not exist:"))
 		})
