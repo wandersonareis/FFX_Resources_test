5dc8be34d824c97718e185d10b1ac6c4101c9527
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-05-14 22:05:56 -0300

    refactor: remover tratamento de erros do serviço de extração

--- frontend/src/service/extract.service.ts
+++ frontend/src/service/extract.service.ts
@@ -1,19 +1,11 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Extract } from '../../wailsjs/go/main/App';
-import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ExtractService {
-  private readonly _errorHandler: ErrorHandlerService =
-    inject(ErrorHandlerService);
-
   async extraction(file: string) {
-    try {
-      await Extract(file);
-    } catch (error) {
-      this._errorHandler.sendErrorNotification(error);
-    }
+    await Extract(file);
   }
 }
