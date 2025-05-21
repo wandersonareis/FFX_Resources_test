4de4ddf2ce5767cdda7f70c1b64744e2edad869d
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-05-21 01:00:19 -0300

    Refines DCP file pattern constants
    Updates the DCP file pattern constants to improve regex accuracy.
    
    Replaces escape sequences with raw string literals for clarity and modifies the file pattern to correctly match the intended file formats.

--- backend/fileFormats/internal/dcp/internal/lib/dcp_constants.go
+++ backend/fileFormats/internal/dcp/internal/lib/dcp_constants.go
@@ -1,6 +1,6 @@
 package lib
 
 const DCP_FILE_BINARY_PARTS_PATTERN string = dcp_file_pattern + "$"
-const DCP_FILE_TXT_PARTS_PATTERN string = dcp_file_pattern + "\\.txt"
+const DCP_FILE_TXT_PARTS_PATTERN string = dcp_file_pattern + `\.txt`
 const DCP_PARTS_TARGET_DIR_NAME string = "system_text"
-const dcp_file_pattern string = "macrodic.*?\\.00[0-6]"
+const dcp_file_pattern string = `macrodic\.(00[0-9]|01[0-5])`
