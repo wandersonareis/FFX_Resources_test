a1183083cbbb20d8356f496d6964706e9b49e1ca
Wanderson A. dos Reis <wandersonfrost8@hotmail.com>
2025-06-14 15:49:35 -0300

    Eliminates redundant path constants related to FFX directory structure.

--- backend/common/constants.go
+++ backend/common/constants.go
@@ -4,15 +4,7 @@ const FFX_DIR_MARKER = "ffx_ps2"
 const FFX_TEXT_FORMAT_SEPARATOR = "--------------------------------------------------End--"
 const (
 	MonsterMaxIndex            = 360
-	PathFfxRoot                = "ffx_ps2/ffx/master/"
 	OriginalsFolder            = "jppc/"
-	PathOriginalsRoot          = PathFfxRoot + OriginalsFolder
-	PathOriginalsKernel        = PathOriginalsRoot + "battle/kernel/"
-	PathMonsterFolder          = PathOriginalsRoot + "battle/mon/"
-	PathOriginalsEncounter     = PathOriginalsRoot + "battle/btl/"
-	PathInternationalEncounter = PathFfxRoot + "inpc/battle/btl/"
-	PathOriginalsEvent         = PathOriginalsRoot + "event/obj/"
-	PathAbmap                  = PathOriginalsRoot + "menu/abmap/"
 	PathTextOutputRoot         = "target/text/"
 	DefaultLocalization        = "us"
 	SkipBlitzballEvents        = true
